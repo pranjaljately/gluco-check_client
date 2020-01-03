6e14d860791d481148f54e91599c6fdee3eb5c25
Pranjal <pjately078@gmail.com>
2020-01-03 18:46:52 +0000

    direction icon based on two latest readings

--- src/components/GlucoseDirectionArrow.js
+++ src/components/GlucoseDirectionArrow.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
 
-const GlucoseDirectionArrow = () => (
-  <Feather name='arrow-up' size={70} style={styles.arrow} />
-);
+const GlucoseDirectionArrow = ({ twoLatestReadings }) => {
+  let name = '';
+  if (twoLatestReadings.length < 2) {
+    name = 'arrow-right';
+  } else {
+    let secondLast = twoLatestReadings[0]['y'];
+    let last = twoLatestReadings[1]['y'];
+    name = iconName(secondLast, last);
+  }
+  return <Feather name={name} size={70} style={styles.arrow} />;
+};
+
+const iconName = (secondLast, last) => {
+  if (secondLast > last) {
+    return 'arrow-down';
+  } else if (secondLast === last) {
+    return 'arrow-right';
+  }
+  return 'arrow-up';
+};
 
 const styles = StyleSheet.create({
   arrow: {
@@ -13,4 +31,9 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
 });
+
+GlucoseDirectionArrow.propTypes = {
+  twoLatestReadings: PropTypes.array.isRequired,
+};
+
 export default GlucoseDirectionArrow;
