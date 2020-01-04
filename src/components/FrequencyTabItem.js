7e8ff1353de4b945c08a60eb8dd70c25abc29616
Pranjal <pjately078@gmail.com>
2020-01-04 22:52:00 +0000

    remove return and add is required to proptype

--- src/components/FrequencyTabItem.js
+++ src/components/FrequencyTabItem.js
@@ -7,35 +7,31 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const FrequencyTabItem = ({ tab, selectedTab, updateSelected }) => {
-  return (
-    <TouchableHighlight
-      onPress={() => updateSelected(tab.id)}
-      style={{
-        alignItems: 'center',
-        flexGrow: 1,
-        flexShrink: 0,
-        flexBasis: 0,
-        marginTop: 5,
-        paddingTop: 17,
-        paddingBottom: 17,
-        borderTopStartRadius: 10,
-        borderTopEndRadius: 10,
-        borderBottomColor: '#FF3A79',
-        // border-radius: 30% / 100%;
-        borderBottomWidth: selectedTab === tab.id ? 2 : 0,
-      }}
-      activeOpacity={0.8}
-      underlayColor={
-        selectedTab === tab.id
-          ? 'rgba(255, 58, 121, 0.3)'
-          : 'rgba(0, 0, 0, 0.3)'
-      }
-    >
-      <Text style={styles.tabText}>{tab.text}</Text>
-    </TouchableHighlight>
-  );
-};
+const FrequencyTabItem = ({ tab, selectedTab, updateSelected }) => (
+  <TouchableHighlight
+    onPress={() => updateSelected(tab.id)}
+    style={{
+      alignItems: 'center',
+      flexGrow: 1,
+      flexShrink: 0,
+      flexBasis: 0,
+      marginTop: 5,
+      paddingTop: 17,
+      paddingBottom: 17,
+      borderTopStartRadius: 10,
+      borderTopEndRadius: 10,
+      borderBottomColor: '#FF3A79',
+      // border-radius: 30% / 100%;
+      borderBottomWidth: selectedTab === tab.id ? 2 : 0,
+    }}
+    activeOpacity={0.8}
+    underlayColor={
+      selectedTab === tab.id ? 'rgba(255, 58, 121, 0.3)' : 'rgba(0, 0, 0, 0.3)'
+    }
+  >
+    <Text style={styles.tabText}>{tab.text}</Text>
+  </TouchableHighlight>
+);
 
 const styles = StyleSheet.create({
   tabText: {
@@ -48,6 +44,7 @@ const styles = StyleSheet.create({
 FrequencyTabItem.propTypes = {
   tab: PropTypes.object.isRequired,
   updateSelected: PropTypes.func.isRequired,
-  selectedTab: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  selectedTab: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
 };
 export default FrequencyTabItem;
