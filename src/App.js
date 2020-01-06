7ce3a3abaa0c3eefe74c75c057ddf7926c6a611c
Pranjal <pjately078@gmail.com>
2020-01-06 13:13:09 +0000

    App.js updated to allow work for next screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
@@ -12,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333333',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
