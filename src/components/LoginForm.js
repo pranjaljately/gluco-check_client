fe847fb2e07e5fb19adee7e768981feac42f9598
Pranjal <pjately078@gmail.com>
2020-01-09 12:29:42 +0000

    Login form - requires request on submit and handling api error messages

---
+++ src/components/LoginForm.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Label, Item, Input, Text, Form } from 'native-base';
+import { View, StyleSheet } from 'react-native';
+import { Formik, ErrorMessage } from 'formik';
+import SubmitButton from './SubmitButton';
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (!values.password) {
+    errors.password = 'Required';
+  }
+
+  return errors;
+};
+
+const LoginForm = () => (
+  <Formik
+    initialValues={{ email: '', password: '' }}
+    onSubmit={({ email, password }) => console.log(email, password)}
+    validate={validate}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values }) => (
+      <Form>
+        <View style={{ marginVertical: 10 }}>
+          <Item floatingLabel last style={styles.formItem}>
+            <Label style={styles.label}>Email</Label>
+            <Input
+              style={styles.input}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+          </Item>
+          <ErrorMessage
+            style={styles.formError}
+            component={Text}
+            name='email'
+          />
+          <Item floatingLabel last style={styles.formItem}>
+            <Label style={styles.label}>Password</Label>
+            <Input
+              style={styles.input}
+              secureTextEntry
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+          </Item>
+          <ErrorMessage
+            style={styles.formError}
+            component={Text}
+            name='password'
+          />
+        </View>
+        <SubmitButton text='Log In' handleSubmit={handleSubmit} />
+      </Form>
+    )}
+  </Formik>
+);
+
+const styles = StyleSheet.create({
+  label: { color: '#FFFFFF' },
+  input: { color: '#FFFFFF' },
+  formError: { color: 'red', fontSize: 15 },
+  formItem: { marginTop: 10 },
+});
+
+export default LoginForm;
