47d4cbade4ec8f2100bb4bd9f9fd0757d89fa045
Pranjal <pjately078@gmail.com>
2020-01-15 00:03:06 +0000

    Small cleanup

--- src/services/AreNotificationsEnabled.js
+++ src/services/AreNotificationsEnabled.js
@@ -2,9 +2,7 @@ import * as Permissions from 'expo-permissions';
 
 const areNotificationsEnabled = async () => {
   const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-  if (status !== 'granted') {
-    return false;
-  }
+  if (status !== 'granted') return false;
 
   return true;
 };
