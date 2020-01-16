9c6347fe99c5e1ade102c76818b65a73dd6a497f
Pranjal <pjately078@gmail.com>
2020-01-16 17:39:42 +0000

    Static insights screen

--- src/screens/InsightsScreen.js
+++ src/screens/InsightsScreen.js
@@ -6,8 +6,8 @@ import FrequencyTabs from '../components/FrequencyTabs';
 
 const tabs = [
   {
-    id: '1hours',
-    text: '1 hour',
+    id: 'daily',
+    text: 'Daily',
     fromTimestampParam() {
       return moment()
         .subtract(1, 'hours')
@@ -15,8 +15,8 @@ const tabs = [
     },
   },
   {
-    id: '6hours',
-    text: '6 hours',
+    id: 'weekly',
+    text: 'Weekly',
     fromTimestampParam() {
       return moment()
         .subtract(6, 'hours')
@@ -24,8 +24,8 @@ const tabs = [
     },
   },
   {
-    id: '12hours',
-    text: '12 hours',
+    id: 'monthly',
+    text: 'Monthly',
     fromTimestampParam() {
       return moment()
         .subtract(12, 'hours')
@@ -33,8 +33,8 @@ const tabs = [
     },
   },
   {
-    id: '24hours',
-    text: '24 hours',
+    id: 'yearly',
+    text: 'Yearly',
     fromTimestampParam() {
       return moment()
         .subtract(24, 'hours')
@@ -59,12 +59,134 @@ const Insights = () => {
   return (
     <SafeAreaView style={styles.container}>
       <TransparentHeader title='Insights' />
-      <View>
-        <FrequencyTabs
-          tabs={tabs}
-          selectedTabId={selectedTab.id}
-          onFrequencyTabPress={onFrequencyTabPress}
-        />
+      <FrequencyTabs
+        tabs={tabs}
+        selectedTabId={selectedTab.id}
+        onFrequencyTabPress={onFrequencyTabPress}
+      />
+      <View
+        style={{
+          padding: 10,
+          marginTop: 7,
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: 'bold',
+            color: '#FFFFFF',
+          }}
+        >
+          23 Mon 2019 - 29 Sun 2019
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+          paddingHorizontal: '5%',
+        }}
+      >
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardTitle}>AVG GLUCOSE</Text>
+          </View>
+          <View>
+            <Text style={{ fontSize: 55, color: '#FF3A79' }}>
+              5.9<Text style={{ fontSize: 20, color: '#FFFFFF' }}>mmol/L</Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardTitle}>ESTIMATED A1C</Text>
+          </View>
+          <View>
+            <Text style={{ fontSize: 60, color: '#FF3A79' }}>
+              6.3<Text style={{ fontSize: 20, color: '#FFFFFF' }}>%</Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardTitle}>LOW BG EVENTS</Text>
+          </View>
+          <View>
+            <Text style={{ fontSize: 60, color: '#FF3A79' }}>
+              4<Text style={{ fontSize: 20, color: '#FFFFFF' }}>times</Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View>
+            <Text style={styles.cardTitle}>HIGH BG EVENTS</Text>
+          </View>
+          <View>
+            <Text style={{ fontSize: 60, color: '#FF3A79' }}>
+              10
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#FFFFFF',
+                }}
+              >
+                times
+              </Text>
+            </Text>
+          </View>
+        </View>
+        <View style={[styles.card, styles.cardLong]}>
+          <View>
+            <Text style={styles.cardTitle}>BG DISTRIBUTION</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+              paddingVertical: 10,
+            }}
+          >
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ fontSize: 60, color: '#FF3A79' }}>10</Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#FFFFFF',
+                  letterSpacing: 2,
+                }}
+              >
+                % LOW
+              </Text>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ fontSize: 60, color: '#FF3A79' }}>65</Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#FFFFFF',
+                  letterSpacing: 2,
+                }}
+              >
+                % IN TARGET*
+              </Text>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ fontSize: 60, color: '#FF3A79' }}>25</Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#FFFFFF',
+                  letterSpacing: 2,
+                }}
+              >
+                % HIGH
+              </Text>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -75,6 +197,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#333333',
   },
+  card: {
+    backgroundColor: '#464646',
+    alignItems: 'center',
+    width: 180,
+    borderRadius: 5,
+    height: 125,
+    marginVertical: 10,
+    elevation: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    justifyContent: 'space-around',
+  },
+  cardLong: {
+    width: '100%',
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+    height: 175,
+  },
+  cardTitle: { color: '#FFFFFF', fontSize: 20, letterSpacing: 2 },
 });
 
 export default Insights;
