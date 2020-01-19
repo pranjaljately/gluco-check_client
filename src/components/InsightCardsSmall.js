324b18f3b4fef8b516a448b3e2c0093259efc5e7
Pranjal <pjately078@gmail.com>
2020-01-19 17:19:40 +0000

    Insight cards small uses api data

--- src/components/InsightCardsSmall.js
+++ src/components/InsightCardsSmall.js
@@ -3,8 +3,40 @@ import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import CardItem from './CardItem';
 
-const InsightCardsSmall = ({ data }) => {
-  const cardItems = data.map(card => <CardItem key={card.id} card={card} />);
+const InsightCardsSmall = ({
+  data: { average, A1C, highEventsCount, lowEventsCount },
+}) => {
+  const cardsData = [
+    {
+      id: 'average',
+      title: 'Avg glucose',
+      value: average,
+      unit: 'mmol/L',
+    },
+    {
+      id: 'A1C',
+      title: 'Estimated A1C',
+      value: A1C,
+      unit: '%',
+    },
+    {
+      id: 'lowEventsCount',
+      title: 'Low BG events',
+      value: lowEventsCount,
+      unit: 'times',
+    },
+    {
+      id: 'highEventsCount',
+      title: 'High BG events',
+      value: highEventsCount,
+      unit: 'times',
+    },
+  ];
+
+  const cardItems = cardsData.map(card => (
+    <CardItem key={card.id} card={card} />
+  ));
+
   return <View style={styles.container}>{cardItems}</View>;
 };
 
@@ -17,6 +49,6 @@ const styles = StyleSheet.create({
 });
 
 InsightCardsSmall.propTypes = {
-  data: PropTypes.array.isRequired,
+  data: PropTypes.object.isRequired,
 };
 export default InsightCardsSmall;
