2b45dc2aba3928bc27a5992590bded93bf7d1e58
Pranjal <pjately078@gmail.com>
2020-01-22 22:59:09 +0000

    Removed - as it was not applied to register/login endpoints

--- src/services/SetAuthToken.js
+++ src/services/SetAuthToken.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
-import Constants from 'expo-constants';
-
-const { manifest } = Constants;
 
 const setAuthToken = token => {
   axios.defaults.headers.common['x-auth-token'] = token;
-
-  // Temp -- needs to be removed for prod
-  const uri = `http://${manifest.debuggerHost.split(':').shift()}:5000`;
-  axios.defaults.baseURL = uri;
 };
 
 export default setAuthToken;
